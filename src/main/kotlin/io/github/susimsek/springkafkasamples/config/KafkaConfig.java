8becd2b83cbc7d629df16eef40377bd35ed41c5e
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-01-08 21:19:19 +0300

    add banner

--- src/main/kotlin/io/github/susimsek/springkafkasamples/config/KafkaConfig.java
+++ src/main/kotlin/io/github/susimsek/springkafkasamples/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package io.github.susimsek.springkafkasamples.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -12,6 +13,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -25,6 +27,15 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @ConditionalOnClass(KafkaTemplate.class)
 @EnableConfigurationProperties(KafkaProperties.class)
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic customerTopic(KafkaProperties kafkaProperties) {
+        var binding = kafkaProperties.getBindings().get("customer");
+        return TopicBuilder.name(binding.getTopic())
+            .partitions(4)
+            .replicas(1)
+            .build();
+    }
 
     @Bean
     public ProducerFactory<String, ?> producerFactory(KafkaConnectionDetails connectionDetails) {
