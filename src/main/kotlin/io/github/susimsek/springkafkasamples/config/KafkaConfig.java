9cc0ff69a0695ad658289a5ffba7612c323fc61c
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-04-20 21:52:47 +0300

    add hateoas sample

--- src/main/kotlin/io/github/susimsek/springkafkasamples/config/KafkaConfig.java
+++ src/main/kotlin/io/github/susimsek/springkafkasamples/config/KafkaConfig.java
@@ -1,10 +1,11 @@
 package io.github.susimsek.springkafkasamples.config;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -23,6 +24,9 @@ import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(KafkaTemplate.class)
 @EnableConfigurationProperties(KafkaProperties.class)
@@ -38,11 +42,12 @@ public class KafkaConfig {
     }
 
     @Bean
-    public ProducerFactory<String, ?> producerFactory(KafkaConnectionDetails connectionDetails) {
+    public ProducerFactory<String, ?> producerFactory(KafkaConnectionDetails connectionDetails,
+                                                      Serializer<?> jsonSerializer) {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, connectionDetails.getProducerBootstrapServers());
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, jsonSerializer.getClass().getName());
         DefaultKafkaProducerFactory<String, ?> factory = new DefaultKafkaProducerFactory<>(config);
         return factory;
     }
@@ -54,17 +59,18 @@ public class KafkaConfig {
     }
 
     @Bean
-    public ConsumerFactory<String, ?> consumerFactory(KafkaConnectionDetails connectionDetails) {
+    public ConsumerFactory<String, ?> consumerFactory(
+        KafkaConnectionDetails connectionDetails,
+        Deserializer<?> jsonDeserializer) {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connectionDetails.getConsumerBootstrapServers());
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, jsonDeserializer.getClass().getName());
         config.put(JsonDeserializer.TRUSTED_PACKAGES, "io.github.susimsek.springkafkasamples.dto");
-        DefaultKafkaConsumerFactory<String, Object> factory = new DefaultKafkaConsumerFactory<>(config);
-        return factory;
+        return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
@@ -73,5 +79,15 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory();
         factory.setConsumerFactory(consumerFactory);
         return factory;
+    }
+
+    @Bean
+    Serializer<?> jsonSerializer(ObjectMapper objectMapper) {
+        return new JsonSerializer<>(objectMapper);
+    }
+
+    @Bean
+    Deserializer<?> jsonDeserializer(ObjectMapper objectMapper) {
+        return new JsonDeserializer<>(objectMapper);
     }
 }
